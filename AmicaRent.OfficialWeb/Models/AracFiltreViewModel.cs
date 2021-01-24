79d3bba6229b8ea4d89a869952a62ae6dcb73b0d
saidyeter <kordiseps@gmail.com>
2021-01-24 23:09:51 +0300

    kurumsal site eldekiler kadarınca ok

---
+++ AmicaRent.OfficialWeb/Models/AracFiltreViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AmicaRent.OfficialWeb.Models
+{
+    public class AracFiltreViewModel
+    {
+        public int alisLokasyon { get; set; }
+        public int donusLokasyon { get; set; }
+        public string alisGun { get; set; }
+        public string alisSaat { get; set; }
+        public string donusGun { get; set; }
+        public string donusSaat { get; set; }
+    }
+}
---
+++ AmicaRent.OfficialWeb/Models/AracViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AmicaRent.OfficialWeb.Models
+{
+    public class AracViewModel
+    {
+        public int Arac_ID { get; set; }
+        public string MarkaModel { get; set; }
+        public double Fiyat { get; set; }
+        public int Indirim { get; set; }
+        public string Yil { get; set; }
+        public int KM { get; set; }
+        public string YakitTuru { get; set; }
+        public string VitesTuru { get; set; }
+        public string Base64Resim { get; set; }
+    }
+}
